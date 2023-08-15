77a742c067e2bb8387e33401ef1e47438197f577
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2023-08-15 16:38:37 -0400

    Make changes requested by linter

--- src/components/fetchData.jsx
+++ src/components/fetchData.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Quotes from './Quotes';
+import Quotes from './Quotes.jsx';
 
 export default function FetchData() {
   const [quote, setQuote] = useState([]);
